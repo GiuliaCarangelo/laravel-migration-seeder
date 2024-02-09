a7593d43be971ddcbec4a63591d7f2c260da47e4
GiuliaCarangelo <tnhkagura@gmail.com>
2024-02-10 00:11:43 +0100

    updated query with filtered departure date and seed

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Train;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
     public function index(){
-        $trains = Train::all();
+        // prendo tutti i treni
+        //$trains = Train::all();
+
+        // prendo solo i treni di oggi
+        //$trains = Train::where('departure_date', Carbon::today())->get();
+        $trains = Train::whereDepartureDate(Carbon::today())->get();
         return view('trains',['treni' => $trains]);
     }  
 }
---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Train;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ( $i = 0; $i <= 20; $i++) {
+            $train = new Train();
+            $train->company = $faker->company();
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure_date = $faker->date();
+            $train->departure_time = $faker->time();
+            $train->arrival_date = $faker->date();
+            $train->arrival_time = $faker->time();
+            $train->train_code = $faker->regexify('[A-Z]{3}[0-9]{3}');
+            $train->total_carriages = $faker->numberBetween(1, 13);
+            $train->on_time = $faker->boolean(50);
+            $train->cancelled = $faker->boolean(10);
+            $train->save();
+        }
+    }
+}
